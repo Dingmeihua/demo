df178715eb18961e6a98f1be683bf0301f154670
mrright <2592535186@qq.com>
2016-10-11 20:54:55 +0800

    sss

--- a.js
+++ a.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2016-10-11 19:33:29
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-10-11 19:55:05
+* @Last Modified time: 2016-10-11 20:48:22
 */
 
 'use strict';
@@ -12,3 +12,4 @@ var b=132;
 function a(){
 	console.log("a");
 }
+var c = 30;
