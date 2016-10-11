cda8ae536759fd8e8ded6a7c17962042d36c3ab3
mrright <2592535186@qq.com>
2016-10-11 19:55:28 +0800

    增加函数

--- a.js
+++ a.js
@@ -2,10 +2,13 @@
 * @Author: Administrator
 * @Date:   2016-10-11 19:33:29
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-10-11 19:48:37
+* @Last Modified time: 2016-10-11 19:55:05
 */
 
 'use strict';
 
 var a=0;
 var b=132;
+function a(){
+	console.log("a");
+}
