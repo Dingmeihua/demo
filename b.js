ec6c493435c09f6b12bca8e5ed45ff7d8c184056
mrright <2592535186@qq.com>
2016-10-11 20:45:27 +0800

    创建b.js

---
+++ b.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Administrator
+* @Date:   2016-10-11 20:44:32
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-10-11 20:44:56
+*/
+
+'use strict';
+function p(){
+	console.log("nihao");
+}
